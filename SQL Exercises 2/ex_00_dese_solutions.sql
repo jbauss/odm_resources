152096f82dc5d86550dbb6f225e8031cce723bb9
Julian Bauß <julian.bauss@gmail.com>
2025-01-23 15:06:41 +0100

    Add files via upload

--- SQL Exercises 2/ex_00_dese_solutions.sql
+++ SQL Exercises 2/ex_00_dese_solutions.sql
@@ -15,8 +15,7 @@ SELECT name FROM districts WHERE name LIKE '%(non-op)';
 -- The Massachusetts Legislature would like to learn how much money, on average, districts spent per-pupil last year. Write a SQL query to find the average per-pupil expenditure. Name the column “Average District Per-Pupil Expenditure”.
 --    Note the per_pupil_expenditure column in the expenditures table contains the average amount, per pupil, each district spent last year. You’ve been asked to find the average of this set of averages, weighting all districts equally regardless of their size.
 
-SELECT AVG(expenditures.per_pupil_expenditure) AS "Average District Per-Pupil Expenditure" FROM districts
-JOIN expenditures ON districts.id = expenditures.district_id;
+SELECT AVG(expenditures.per_pupil_expenditure) AS "Average District Per-Pupil Expenditure" FROM expenditures;
 
 -- 4.)
 -- Some cities have more public schools than others. Write a SQL query to find the 10 cities with the most public schools. Your query should return the names of the cities and the number of public schools within them, ordered from greatest number of public schools to least. If two cities have the same number of public schools, order them alphabetically.
